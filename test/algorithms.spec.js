4f0e2653beb822564424993d9d1828e176a87261
AndyL <missedmemo@gmail.com>
2016-09-16 20:21:57 -0700

    refactor test variable names, with additional unit tests

--- test/algorithms.spec.js
+++ test/algorithms.spec.js
@@ -3,21 +3,34 @@ import * as _ from '../algorithms';
 
 describe( 'Sorting Algorithms...', () => {
 
-  let numeric, sortedNumeric;
+  let tuple,     sortedTuple,
+      numeric,   sortedNumeric,
+      preSorted, sortedPreSorted,
+      negValues, sortedNegValues,
+      reversed,  sortedReversed,
+      decimals,  sortedDecimals;
+
   beforeEach( () => {
-    numeric = [2,5,3,4,6,1], sortedNumeric = [1,2,3,4,5,6];
+    tuple   = [3,2],                  sortedTuple = [2,3],
+    numeric = [10,5,3,8,2,6,4,7,9,1], sortedNumeric = [1,2,3,4,5,6,7,8,9,10];
   });
 
   const sorts = [
     { description: 'bubbleSort: O(n^2)', method: _.bubbleSort }
   ];
   
-  sorts.forEach( algorithm => {
+  sorts.forEach( sort => {
 
-    describe( algorithm.description, () => {
-  
-      it( 'should sort a basic numeric array', () => {
-        expect( algorithm.method(numeric) ).to.deep.equal(sortedNumeric);
+    describe( sort.description, () => {
+
+      it( 'should switch values of two-member array', () => {
+        let results = sort.method( tuple );
+        expect( results ).to.deep.equal( sortedTuple );
+      });
+
+      it( 'should sort a simple numeric array', () => {
+        let results = sort.method( numeric );
+        expect( results ).to.deep.equal( sortedNumeric );
       });
 
     });
