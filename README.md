a8cde58789fecb3f3bfe2de118f01a5b7f5ac035
AndyL <missedmemo@gmail.com>
2016-09-16 18:55:36 -0700

    implemented initial algorithm (bubbleSort)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Algorithm Testing in Mocha/Chai & ES6
 
-### dynamically generate a suite of tests to apply against a set of algorithms
+### dynamically generate a suite of tests to apply against a growing set of algorithms
 
 
 Demonstrate succinct versions of several standard sorting algorithms, along with the absolute MINIMAL associated infrastructure required to carry out effective mocha/chai testing under ES6
--- algorithms.js
+++ algorithms.js
@@ -1,4 +1,20 @@
 export const bubbleSort = (array) => {
-  return [1,2,3];
-}
+  
+  do {
+
+    var swapped = false; // cannot use `let` here!
+
+    for( let i = 0, temp; i < array.length; i++ ) {
+      if( array[ i + 1 ] < array[i] ) {
+        temp = array[i];
+        array[i] = array[ i + 1 ];
+        array[ i + 1 ] = temp;
+        swapped = true;
+      }
+    }
+
+  } while( swapped );
+
+  return array;
+};
 
