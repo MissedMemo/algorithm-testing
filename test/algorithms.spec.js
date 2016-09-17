ce23e4b9b82a292a925bec7791643f5ad1eeaa4f
AndyL <missedmemo@gmail.com>
2016-09-16 20:42:51 -0700

    define additional unit tests

--- test/algorithms.spec.js
+++ test/algorithms.spec.js
@@ -11,8 +11,11 @@ describe( 'Sorting Algorithms...', () => {
       decimals,  sortedDecimals;
 
   beforeEach( () => {
-    tuple   = [3,2],                  sortedTuple = [2,3],
-    numeric = [10,5,3,8,2,6,4,7,9,1], sortedNumeric = [1,2,3,4,5,6,7,8,9,10];
+    tuple     = [3,2],                  sortedTuple     = [2,3],
+    numeric   = [10,5,3,8,2,6,4,7,9,1], sortedNumeric   = [1,2,3,4,5,6,7,8,9,10],
+    preSorted = [1,2,3,4,5],            sortedPreSorted = [1,2,3,4,5],
+    negValues = [1,2,-3,4,-5],          sortedNegValues = [-5,-3,1,2,4],
+    reversed  = [5,4,3,2,1],            sortedReversed  = [1,2,3,4,5];
   });
 
   const sorts = [
@@ -34,6 +37,21 @@ describe( 'Sorting Algorithms...', () => {
       it( 'should sort a simple numeric array', () => {
         let results = sort.method( numeric );
         expect( results ).to.deep.equal( sortedNumeric );
+      });
+
+      it( 'should handle presorted arrays', () => {
+        let results = sort.method( preSorted );
+        expect( results ).to.deep.equal( sortedPreSorted );
+      });
+      
+      it( 'should sort arrays containing negative numbers', () => {
+        let results = sort.method( negValues );
+        expect( results ).to.deep.equal( sortedNegValues );
+      });
+
+      it( 'should handle reverse-sorted arrays', () => {
+        let results = sort.method( reversed );
+        expect( results ).to.deep.equal( sortedReversed );
       });
 
     });
