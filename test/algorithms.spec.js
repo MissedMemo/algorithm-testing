04e64b3ee7751a9a08e23e63fc49c08347bc602f
AndyL <missedmemo@gmail.com>
2016-09-16 19:07:37 -0700

    implement logic to dynamically generate tests against list of algorithms

--- test/algorithms.spec.js
+++ test/algorithms.spec.js
@@ -1,12 +1,25 @@
 import {expect} from 'chai';
 import * as _ from '../algorithms';
 
-describe( 'Sorting algorithms...', () => {
+describe( 'Sorting Algorithms...', () => {
+
+  let numeric, sortedNumeric;
+  beforeEach( () => {
+    numeric = [2,5,3,4,6,1], sortedNumeric = [1,2,3,4,5,6];
+  });
+
+  const sorts = [
+    { description: 'bubbleSort: O(n^2)', method: _.bubbleSort }
+  ];
   
-  describe( 'bubbleSort:', () => {
+  sorts.forEach( algorithm => {
+
+    describe( algorithm.description, () => {
   
-    it( 'should sort a basic numeric array', () => {
-      expect( _.bubbleSort([2,3,1]) ).to.deep.equal([1,2,3]);
+      it( 'should sort a basic numeric array', () => {
+        expect( algorithm.method(numeric) ).to.deep.equal(sortedNumeric);
+      });
+
     });
 
   });
